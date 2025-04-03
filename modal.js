cab4b8d440cdb7e178d6bcc84498426a5e93851b
kendes05 <ptimbo652@gmail.com>
2025-04-03 15:20:51 -0300

    filmes sendo adicionados

--- modal.js
+++ modal.js
@@ -1,5 +1,25 @@
 const background = document.getElementById("background");
+const addToListButton = document.getElementById("add-to-list");
+
+let currentMovie = {};
 
 background.addEventListener("click", () => {
   overlay.classList.remove("visible");
 });
+
+function modalConstructor(data) {
+  currentMovie = data;
+  movieTitle.textContent = data.Title + " - " + data.Year;
+  moviePoster.src = data.Poster;
+  movieCast.textContent = data.Actors;
+  moviePlot.textContent = data.Plot;
+  movieGenre.textContent = data.Genre;
+}
+
+function addToListButtonHandler() {
+  updateUI(currentMovie);
+  addToList(currentMovie);
+  overlay.classList.remove("visible");
+}
+
+addToListButton.addEventListener("click", addToListButtonHandler);
--- script.js
+++ script.js
@@ -7,21 +7,52 @@ const moviePoster = document.getElementById("movie-poster");
 const moviePlot = document.getElementById("movie-plot");
 const movieCast = document.getElementById("movie-cast");
 const movieGenre = document.getElementById("movie-genre");
+const movieList = document.getElementById("movie-list");
+
+let movieArray = [];
 
 searchButton.addEventListener("click", searchButtonClickHandler);
 
 async function searchButtonClickHandler() {
-  const movieNameValue = movieNameInput.value.trim().split(" ").join("+");
-  const movieYearValue = movieYearInput.value.trim();
-  let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieNameValue}&y=${movieYearValue}`;
+  if (movieNameInput.value == "") {
+    notie.alert({ text: "Preencha o nome do filme", type: "error" });
+    return;
+  }
+
+  let url = urlGenerator();
+
   const response = await fetch(url);
   const data = await response.json();
+
+  console.log(data.Response);
+  if (data.Response == "False") {
+    notie.alert({ text: "Filme não encontrado", type: "error" });
+    return;
+  }
+
+  modalConstructor(data);
+
   console.log(data);
-  movieTitle.textContent = data.Title + " - " + data.Year;
-  moviePoster.src = data.Poster;
-  movieCast.textContent = data.Actors;
-  moviePlot.textContent = data.Plot;
   overlay.classList.add("visible");
 }
 
-//http://www.omdbapi.com/?apikey=[yourkey]&
+function urlGenerator() {
+  const movieNameValue = movieNameInput.value.trim().split(" ").join("+");
+  const movieYearValue = movieYearInput.value.trim();
+  return `http://www.omdbapi.com/?apikey=${key}&t=${movieNameValue}&y=${movieYearValue}`;
+}
+
+function addToList(movieObject) {
+  movieArray.push(movieObject);
+}
+function updateUI(movieObject) {
+  movieList.innerHTML += `<div class="my-movie">
+          <img
+            src="${movieObject.Poster}"
+            alt=""
+          />
+          <button class="remove-button">
+            <i class="bx bx-trash"></i> Remover
+          </button>
+        </div>`;
+}
